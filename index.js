3052500d0368523629e22c16dd366eab05ffbea8
Kobe Foster <kobegotcha280@gmail.com>
2025-02-25 14:38:04 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("bye");
+console.log(" I like pineapple on pizza?");
+
+window.alert("Payload sucessfully delivered for MacBook Air");
+
+
+document.getElementById("h1,1").textContent = 'Done for bro';
+document.getElementById("firstpara").textContent = 'LIKE I MEAN DONE FOR'
+window.alert("All system changes cannout be un-done, system injection confirmed.");
+
+let colors = ["#ff0000", "#0000ff"]; // Red and Blue
+let index = 0;
+
+setInterval(() => {
+    document.body.style.backgroundColor = colors[index];
+    index = (index + 1) % colors.length;
+}, 25); 
+ 
